063daab84d16e6f12e2e6f9ee0a0850374dc8fe0
clemsclement <clemsclement@hotmail.fr>
2024-07-26 11:57:17 +0200

    env

--- env.php
+++
@@ -1,8 +0,0 @@
-<?php 
-// Ce fichier nous permettras de stocker les variables d'environnements comme les identifiants de connexion à notre BDD, nous mettrons ce fichier dans le fichier .gitignore afin de ne pas le rendre public lors de nos push.
-
-$_ENV['hostBDD'] = 'localhost';
-$_ENV['dbnameBDD'] = 'marinade';
-$_ENV['dbLogin'] = 'root';
-$_ENV['dbPassword'] = '';
-?>
