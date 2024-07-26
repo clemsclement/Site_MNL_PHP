050cb2342938950de4add6eeb7cbea9356517c2a
clemsclement <clemsclement@hotmail.fr>
2024-07-26 16:26:16 +0200

    header

--- view/viewHeader.php
+++ view/viewHeader.php
@@ -9,24 +9,28 @@ function renderHeader(){
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Marinade et Limonade</title>
-    <link rel="stylesheet" href="./Site_MNL_PHP/css/style.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link
+        href="https://fonts.googleapis.com/css2?family=Amatic+SC:wght@400;700&family=Space+Grotesk:wght@700&display=swap"
+        rel="stylesheet">
+    <link rel="stylesheet" href="./css/style.css">
 
 </head>
 <body>
     <header>
-        <h1>Test</h1>
         <nav>
             <ul>
                 <li><a href="/Site_MNL_PHP">ACCUEIL</a></li>
                 <li><a href="/Site_MNL_PHP/lacarte">LA CARTE</a></li>
                 <li><a href="/Site_MNL_PHP/apropos">À PROPOS</a></li>
-                <li><img src="" alt="Logo du Restaurant Marinade et Limonade"></li>
+                <li><img src="./images/logo.jpg" alt="Logo du Restaurant Marinade et Limonade"></li>
                 <li><a href="/Site_MNL_PHP/lesRecettes">RECETTE</a></li>
                 <li><a href="/Site_MNL_PHP/contact">CONTACT</a></li>
-                <div class="iconeNavBar">
-                    <a href="/Site_MNL_PHP/panier"><img src="" alt="Icône Panier"></a>
-                    <a href="/Site_MNL_PHP/login"><img src="" alt="Icône Login"></a>
-                </div>
+                <li class="iconeNavBar">
+                    <a href="/Site_MNL_PHP/panier"><img src="./images/panier.png" alt="IcôneHeader Panier"></a>
+                    <a href="/Site_MNL_PHP/login"><img src="./images/connexion.png" alt="IcôneHeader Login"></a>
+</li>
             </ul>
         </nav>
     </header>
